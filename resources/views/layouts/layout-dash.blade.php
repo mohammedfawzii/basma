87dc8512097e4ffe78d179a261690a27fa837010
mohammedfawzii <mo.fawzi.abz@gmail.com>
2024-07-21 23:38:11 +0300

    add layout dashboard

--- resources/views/layouts/layout-dash.blade.php
+++ resources/views/layouts/layout-dash.blade.php
@@ -10,23 +10,23 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{asset("assets/dashboard/img/favicon.png")}}" rel="icon">
+    <link href="{{asset("assets/dashboard/img/apple-touch-icon.png")}}" rel="apple-touch-icon">
 
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet">
 
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/bootstrap-icons/bootstrap-icons.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/boxicons/css/boxicons.min.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/quill/quill.bubble.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/quill/quill.snow.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/simple-datatables/style.css")}}" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/vendor/remixicon/remixicon.css")}}" rel="stylesheet">
 
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="{{asset("assets/dashboard/css/style.css")}}" rel="stylesheet">
 
 </head>
 
@@ -62,15 +62,15 @@
 
 
 
-    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="assets/vendor/echarts/echarts.min.js"></script>
-    <script src="assets/vendor/quill/quill.js"></script>
-    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="{{asset("assets/dashboard/vendor/apexcharts/apexcharts.min.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/chart.js/chart.umd.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/echarts/echarts.min.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/quill/quill.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/simple-datatables/simple-datatables.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/tinymce/tinymce.min.js")}}"></script>
+    <script src="{{asset("assets/dashboard/vendor/php-email-form/validate.js")}}"></script>
+    <script src="{{asset("assets/dashboard/js/main.js")}}"></script>
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/layout-dash');
 });
 
 Auth::routes();
